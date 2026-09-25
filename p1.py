a0671f488b100fecd8c8975c7c95ec71cfbd46dc
naniwoitteirunoka-wakaranai <lol262711@gmail.com>
2026-09-25 21:13:20 +0530

    eh

--- p1.py
+++ p1.py
@@ -1,29 +1,33 @@
 # Simple Password Strength Checker
 password = input("Enter password ")
 if len(password) >= 8:
-print("Password length is good")
+   print("Password length is good")
 else:
-print("Weak password, Use at least 8 characters")
+   print("Weak password, Use at least 8 characters")
+  
 # Introduction to TryHackMe:Basic User Authentication Simulation
 username = input("Enter username ")
 password = input("Enter password ")
 if username == "cat" and password == "meow1234":
-print("Login Successful")
+   print("Login Successful")
 else:
-print("Invalid Username or Password")
+   print("Invalid Username or Password")
+  
 # Find Local IP Address
 import socket
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 print("Hostname ", hostname)
 print("Local IP Address ", ip_address)
+
 # Simple Caesar Cipher
 text = input("Enter message ")
 shift = 3
 encrypted = ""
 for char in text:
-encrypted += chr(ord(char) + shift)
+    encrypted += chr(ord(char) + shift)
 print("Encrypted Message ", encrypted)
+
 # Hex Encoding and Decoding
 text = "Hello"
 encoded = text.encode().hex()
